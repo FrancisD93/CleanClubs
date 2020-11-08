8a92d550e2ff0f4a5cc50815aafd85259c3c041c
Sean <srafferty89@gmail.com>
2020-11-08 14:07:23 +0000

    new exception controlled controller endpoints

--- src/Services/Clubs/Clubs.API/Controllers/UserController.cs
+++ src/Services/Clubs/Clubs.API/Controllers/UserController.cs
@@ -7,5 +7,51 @@ namespace Clubs.API.Controllers
         {
             _Logger = logger;
         }
+
+        #region GET
+
+        /// <summary>
+        /// Query all Users
+        /// </summary>
+        /// <returns>Collection of TODO records</returns>
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public async Task<IActionResult> Get()
+        {
+            _Logger.LogInformation($"User: {HelperMethods.GetCallerMemberName()}");
+            throw new NotImplementedException();
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id">unique Guid for a user</param>
+        /// <returns>A single user record</returns>
+        [HttpGet("{id}", Name = "GetUserById")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public async Task<IActionResult> GetUserById(Guid id)
+        {
+            _Logger.LogInformation($"User: {HelperMethods.GetCallerMemberName()}");
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id">unique Guid for a user</param>
+        /// <returns>A single user record</returns>
+        [HttpGet("{id}", Name = "GetUserByObjectId")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public async Task<IActionResult> GetUserByObjectId(Guid id)
+        {
+            _Logger.LogInformation($"User: {HelperMethods.GetCallerMemberName()}");
+            throw new NotImplementedException();
+        }
+
+        #endregion
     }
 }
